71cf8d621511da67d77443f21597368ea87148a1
GenThrive <genthrive@ecorise.org>
2025-03-13 13:28:34 -0500

    added features

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,37 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+
+import Places from "./Places.jsx";
+
+const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const data = await response.json();
+      setAvailablePlaces(data.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+
+    // fetch("http://localhost:3000/places")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setAvailablePlaces(data.places);
+    //   });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Places are Loading..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
